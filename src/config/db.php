62dc4949c3816d7dd57386acec063dcf258ba969
olatzeu <segurtasuna.oeu.2025@gmail.com>
2025-10-27 19:58:01 +0100

    Update db.php

--- src/config/db.php
+++ src/config/db.php
@@ -10,13 +10,11 @@ $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (PDOException $e) {
-    // Si la conexión falla, se detiene el script con un mensaje
-    die("Ezin izan da konektatu datu-basera: " . $e->getMessage());
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
