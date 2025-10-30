31669677d93a1d34d1a67d81dc067a37b60a7fd8
olatzeu <segurtasuna.oeu.2025@gmail.com>
2025-10-30 12:48:56 +0100

    Update register.php

--- src/register.php
+++ src/register.php
@@ -76,6 +76,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>register</title>
+    <link rel="icon" type="image/png" href="images/button.png">
     <link rel="stylesheet" href="assets/styleHas.css">
 </head> 
 <body>
