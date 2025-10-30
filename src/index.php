85d98982ec40b31bc31c1699243dc21d18e9c0db
olatzeu <segurtasuna.oeu.2025@gmail.com>
2025-10-30 12:39:38 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -40,7 +40,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Maskoti</title>
     <link rel="stylesheet" href="assets/styleHome.css">
 </head>
 <body>
